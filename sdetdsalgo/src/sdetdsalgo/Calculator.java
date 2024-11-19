9f7ef7f597b485e0b075ca29ae73f97783281eba
Rajasri <Rajasri.kr@gmail.com>
2024-11-19 13:38:09 -0600

    Calculator class added

---
+++ sdetdsalgo/src/sdetdsalgo/Calculator.java
@@ -0,0 +1,12 @@
+package sdetdsalgo;
+
+public class Calculator {
+	
+	 int FirstNumber;
+	 int SecondNumber;
+	 int Result;
+	 
+	 public void Addition() {
+			Result = FirstNumber + SecondNumber;
+	}
+}
--- sdetdsalgo/src/sdetdsalgo/PrintTest.java
+++ sdetdsalgo/src/sdetdsalgo/PrintTest.java
@@ -3,6 +3,20 @@ package sdetdsalgo;
 public class PrintTest {
 public static void main(String[] args) {
 	System.out.println("hi this is the first push");
-        System.out.println("hi this is the second push")
+        System.out.println("hi this is the second push");
+        
+        
+       
+        PrintTest ps = new PrintTest();
+        ps.Calculate();
 }
+
+
+	public void Calculate() {
+		Calculator cal = new Calculator();
+		cal.FirstNumber = 10;
+		cal.SecondNumber = 15;
+		cal.Addition();
+		System.out.println("The Result of Addition is :" + cal.Result);
+	}
 }
