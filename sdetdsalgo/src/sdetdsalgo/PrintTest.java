2e2aef1ef9f5816f82c684ff1ae0856e0ec0d0d7
SDET185QA@gmail.com <SDET185QA@gmail.com>
2024-11-19 12:08:39 -0500

    second commit

--- sdetdsalgo/src/sdetdsalgo/PrintTest.java
+++ sdetdsalgo/src/sdetdsalgo/PrintTest.java
@@ -3,5 +3,6 @@ package sdetdsalgo;
 public class PrintTest {
 public static void main(String[] args) {
 	System.out.println("hi this is the first push");
+        System.out.println("hi this is the second push")
 }
 }
