f1056bd58484fe90f5483668d2ad0e5f00129685
dsalgoproject <swasthika.cse@gmail.com>
2024-11-19 21:33:57 -0500

    "Swas first commit"

---
+++ New_Try/src/test/java/newtest/NewTest.java
@@ -0,0 +1,11 @@
+package newtest;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+	@Test
+	public void print() {
+		System.out.println("Hellllllo!!!!!!");
+	}
+
+}
---
+++ sdetdsalgo/src/sdetdsalgo/src/test/java/newtest/NewTest.java
@@ -0,0 +1,8 @@
+package newtest;
+
+public class NewTest {
+	public void print() {
+		System.out.println("Hellllllo!!!!!!");
+	}
+
+}
