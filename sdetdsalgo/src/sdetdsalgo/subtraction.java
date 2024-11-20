010adf45a5855dc1ef68bed3f662f62b2036d956
HaripriyaVijayakumar <haripriya.hemnath@gmail.com>
2024-11-19 19:04:40 -0500

    class subtraction is added

---
+++ sdetdsalgo/src/sdetdsalgo/subtraction.java
@@ -0,0 +1,12 @@
+package sdetdsalgo;
+
+public class subtraction {
+	public void Calculate() {
+		Calculator cal = new Calculator();
+		cal.FirstNumber = 10;
+		cal.SecondNumber = 15;
+		cal.subtraction();
+		System.out.println("The Result of Addition is :" + cal.Result);
+	}
+}
+}
